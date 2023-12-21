caf9bd9e694cb459d08f08811354988d077fe7b2
AndreSci <scitecever@gmail.com>
2023-12-21 16:49:38 +0300

    small fix

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import requests
 import sys
 from misc.logger import Logger
 
+# Адрес и порта расположения RTSP server
 URL = "192.168.15.10"
 PORT = 8093
+
 LOG_PATH = None  # Возможно указать путь в строке - пример: "/home/asterisk/logs/"
+# (предварительно требуется создание полного пути)
 
 logger = Logger(log_path=LOG_PATH)
 
 
-def do_request(caller_id=205, answer_id=200):
-
+def do_request(caller_id: int, answer_id: int):
+    """ Функция отправляет запрос на RTSP сервер """
     try:
         res = requests.get(f"http://{URL}:{PORT}/"
                            f"action.save?caller_id={caller_id}&answer_id={answer_id}", timeout=2)
@@ -20,6 +23,8 @@ def do_request(caller_id=205, answer_id=200):
         logger.event(f"Результаты запроса: {res}")
     except Exception as ex:
         logger.exception(f"Исключение вызвало: {ex}")
+
+    return True
 
 
 def main():
@@ -37,11 +42,13 @@ def main():
             logger.warning(f"Звонящий не может быть инициатором нажатия клавиши: {caller_id} == {answer_id}")
     except Exception as ex:
         logger.exception(f"Исключение вызвало: {ex}")
+    finally:
+        # Сделано для теста в Debug mode.
+        if sys.gettrace() is None:
+            do_request(205, 200)
 
 
 if __name__ == "__main__":
-    # logger.event(f"Запуск скрипта", print_it=False)
-    # main()
+    logger.event(f"Запуск скрипта", print_it=False)
 
-
-    do_request()
+    main()
